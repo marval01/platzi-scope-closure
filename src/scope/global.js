5b7ee2a12e2051064f4b60d15df5564702a5f6f3
martincv <sonmartin94@gmail.com>
2022-12-22 23:30:53 -0500

    Inicializacion de nuestro proyecto

---
+++ src/scope/global.js
@@ -0,0 +1,23 @@
+// variables
+
+var a; // declaramos
+var b = 'b'; // declaramos/asignamos
+b = 'bb'; //reasignacion
+var a = 'aa'; //redeclaracion
+
+//Global Scope
+var fruit = 'Apple'; //global
+
+function bestFruit(){
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries(){
+    country = "Colombia";
+    console.log(country);
+}
+
+countries();
+console.log(country);
