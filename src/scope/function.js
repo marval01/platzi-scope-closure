198d3723e2199cc42980c4c30d4d914abbee7618
martincv <sonmartin94@gmail.com>
2022-12-23 00:06:39 -0500

    Function scope y Block scope

---
+++ src/scope/function.js
@@ -0,0 +1,11 @@
+function greeting(){
+    let useName = 'Ana';
+    console.log(userName);
+
+    if(userName === 'Ana'){
+        console.log(`Hello ${userName}!`)
+    }
+}
+
+greeting();
+console.log(userName);
