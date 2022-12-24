0c9a1f6dea69479656d0139793024fce5aad6266
martincv <sonmartin94@gmail.com>
2022-12-24 13:09:28 -0500

    Reasignación, redeclaracion y Strict Mode

--- src/scope/block.js
+++ src/scope/block.js
@@ -1,11 +1,13 @@
 function fruits(){
     if(true){
-        var fruit1 = 'Apple';
-        let fruit2 = 'Kiwi';
-        const fruit3 = 'Banana';
+        var fruit1 = 'Apple'; // function scope
+        let fruit2 = 'Kiwi'; // block scope
+        const fruit3 = 'Banana'; // block scope
     }
 
     console.log(fruit1);
     console.log(fruit2);
     console.log(fruit3);
 }
+
+fruits();
---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName; // Undefined
+firstName = 'Martin';
+
+console.log(firstName);
+
+var lastName = 'Alonso'; // declarar / asignar
+lastName = 'Jose'; // reasignar
+
+console.log(lastName);
+
+var secondName = 'Alonso'; //declarando / asignando
+var secondName = 'Ana'; // redeclaracion
+
+console.log(secondName);
+
+
+
+// Let
+let fruit = 'Apple'; // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+
+// Const
+const animal = 'dog'; // declara y asigna
+animal = 'cat'; // no se puede reasignar
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('Toyota');
+
+console.log(vehicles);
+
+vehicles.unshift('Ford');
+console.log(vehicles);
